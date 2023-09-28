175d0d42abb8a5679c893bd0eb4d7d0b2142375a
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2023-09-28 17:23:11 -0300

    feat: Atualizada hash e consertada a parte que permite passar uma hash diferente da Dockerimage.

--- go_herbie.sh
+++ go_herbie.sh
@@ -10,11 +10,11 @@ progname=${0##*/}
 go() {
 	docker_path="`command -v docker`"
 	program_to_run="`readlink -f $1 2>/dev/null`"
-	copadocker_image_hash='sha256:cfe962e9b5850bff2296c1bb462d9bca25702deab2b61a9b30503493c0fab805'
+	copadocker_image_hash='sha256:fc28c6cabfb650fc4587482b7b1ae3fe849507441d3203b9377540fa6a468698'
 	
 	[ -z $program_to_run ] && usage
 
-	docker run -v "$program_to_run:/var/rinha/source.rinha.json" ${copadocker_image_hash:-$2}
+	docker run -v "$program_to_run:/var/rinha/source.rinha.json" ${2-$copadocker_image_hash}
 }
 
 usage() {
